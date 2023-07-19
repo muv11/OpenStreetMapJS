babd55ff8a34efc0fdb2adc7f2a9d12c0b38d254
muv11 <uliamozgovaya03@gmail.com>
2023-07-19 13:15:40 +0500

    Create functions for getting features from request and adding user layer

---
+++ jsonController.js
@@ -0,0 +1,23 @@
+async function getContactMapFeatures() {
+    const response = await fetch('http://ipUrl/json/map.json');
+    return response.json().then((data) => {
+        return data;
+    });
+}
+
+async function addUserLayer() {
+    let userGeoJson = await getContactMapFeatures();
+    let vectorSource = new ol.source.Vector({
+        features: new ol.format.GeoJSON().readFeatures(userGeoJson, {
+            dataProjection: 'EPSG:4326',
+            featureProjection: 'EPSG:3857'
+        })
+    });
+
+    let userVectorLayer = new ol.layer.Vector({
+        source: vectorSource
+    });
+
+    map.addLayer(userVectorLayer);
+}
+addUserLayer().then();
